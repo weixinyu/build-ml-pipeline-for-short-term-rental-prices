f6100ab498d6a561b41ab744292a3e70dc9f6433
weixinyu <weixinyu2012@126.com>
2023-05-11 01:46:06 +0000

    add the clean data step

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -5,6 +5,7 @@ Performs basic cleaning on the data and save the results in Weights & Biases
 import argparse
 import logging
 import wandb
+import pandas as pd
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
@@ -13,44 +14,85 @@ logger = logging.getLogger()
 
 def go(args):
 
-    run = wandb.init(project='my_project', job_type="basic_cleaning", group="experiment_1")
+    run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
+    
+    
 
     # Download input artifact. This will also log that this script is using this
     # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    logger.info("Downloading artifact")
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    df = pd.read_csv(artifact_local_path)
+    
+    logger.info("Cleaning data")
+    # Drop outliers
+    min_price = args.min_price
+    max_price = args.max_price
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx].copy()
+    # Convert last_review to datetime
+    df['last_review'] = pd.to_datetime(df['last_review'])
+    
+    logger.info("Creating artifact")
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
-
+    df.to_csv("clean_sample.csv")
+    
+    artifact = wandb.Artifact(
+     args.output_artifact,
+     type=args.output_type,
+     description=args.output_description
+    )
+    
+    logger.info("Logging artifact")
+    artifact.add_file("clean_sample.csv")
+    run.log_artifact(artifact)
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="This steps cleans the data")
 
-
     parser.add_argument(
-        "--parameter1", 
+        "--input_artifact", 
         type=str,
-        help="parameter1",
+        help="Fully-qualified artifact name for the input artifact",
         required=True
     )
 
     parser.add_argument(
-        "--parameter2", 
+        "--output_artifact", 
         type=str,
-        help="parameter2",
+        help="Name for the W&B artifact that will be created",
         required=True
     )
 
     parser.add_argument(
-        "--parameter3", 
+        "--output_type", 
         type=str,
-        help="parameter3",
+        help="Type of the artifact to create",
         required=True
     )
-
+    
+    parser.add_argument(
+        "--output_description", 
+        type=str,
+        help="Description for the artifact",
+        required=True
+    )
+    
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help="min price of the price column",
+        required=True
+    )
+    
+    parser.add_argument(
+        "--max_price", 
+        type=float,
+        help="max price of the price column",
+        required=True
+    )
 
     args = parser.parse_args()
 
